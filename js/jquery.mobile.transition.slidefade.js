414a9bd20fd3de33db97556407f2e6347b99a0ee
forresst <forresst@voila.fr>
2012-03-30 09:10:53 +0200

    Traduction&Maj(http://tinyurl.com/89ucsf4)

--- js/jquery.mobile.transition.slidefade.js
+++ js/jquery.mobile.transition.slidefade.js
@@ -1,22 +1,19 @@
 /*
-* fallback transition for slide in non-3D supporting browsers (which tend to handle complex transitions poorly in general
+* fallback transition for slidefade in non-3D supporting browsers (which tend to handle complex transitions poorly in general
 */
 
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
 //>>description: Handler and fallback transition for slide in non-3D supporting browsers
 //>>label: Slide handler and fallback transition
 //>>group: Transitions
-//>>css: ../css/structure/jquery.mobile.transition.slide.css
+//>>css: ../css/structure/jquery.mobile.transition.slidefade.css
 
 define( [ "jquery", "./jquery.mobile.transition" ], function( $ ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, window, undefined ) {
 
-// Use the simultaneous transition handler for slide transitions
-$.mobile.transitionHandlers.slide = $.mobile.transitionHandlers.simultaneous;
-
 // Set the slide transition's fallback to "fade"
-$.mobile.transitionFallbacks.slide = "fade";
+$.mobile.transitionFallbacks.slidefade = "fade";
 
 })( jQuery, this );
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
